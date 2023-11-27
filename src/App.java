27a4908561ce4e622fceefb91c48be338b26502b
Axel Sjöholm <Axel.sjoholm@elev.ga.ntig.se>
2023-11-27 08:51:01 +0100

    asda

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        radie(9);
+    }
+
+    public static void radie(int radie1) {
+        System.out.println(radie1 * 3.14);
+    }
+}
